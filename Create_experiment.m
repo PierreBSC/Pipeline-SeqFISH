ab4593e30fb1963ce6cde7eaa4f2deb7a424e4a7
Florian Mueller <muellerf.research@gmail.com>
2020-01-23 12:29:09 +0100

    Improved user experience when defining an experiment
    When canceling one of the dialogs, the definition of experiments will now stop.

--- Create_experiment.m
+++ Create_experiment.m
@@ -1,29 +1,32 @@
 function [Analysis_result,Parameters] = Create_experiment()
+Analysis_result = {};
+Parameters = {};
+
 %First function of the pipeline 
 %Create the different variables, load the settings for the analysis
 
-%Geting the main image directory
+% Aks user for the main image directory
 Image_directory = uigetdir('title','Provide the path to the Image Folder');
+if Image_directory == 0; return; end
 
-
-%Geting the parameter for spot detection through a user Interface
+% Geting the parameter for spot detection through a user Interface
 Parameters = Define_parameters_spot_detection();
+if isempty(Parameters); return; end
 
-%Adding parameters for cell segmentation
-
+% Adding parameters for cell segmentation
 Parameters = Define_parameters_cell_segmentation(Parameters);
-
+if isempty(Parameters); return; end
 
 %Geting the matrix design and loading it
-
 Matrix_design = Define_matrix_design();
+if isempty(Matrix_design); return; end
+
 Parameters.Matrix_design = Matrix_design;
 Parameters.N_round = size(Matrix_design,1);
 Parameters.N_channel = size(Matrix_design,2);
 Parameters.Image_directory = Image_directory;
 
 %Creating the variable containing all the results of analysis
-
 Analysis_result = struct;
 
 end
--- Define_matrix_design.m
+++ Define_matrix_design.m
@@ -9,9 +9,6 @@ definput = {'1' '4'};
 
 answer = inputdlg(prompt,dlgtitle,dims,definput);
 
-
-
-
 if ~ isempty(answer)
     
     % Read parameters
@@ -28,7 +25,7 @@ if ~ isempty(answer)
     prompt = 1:N_Rounds*N_Channels;
     prompt = num2cell(prompt);
     prompt = cellfun(@num2str,prompt,'UniformOutput',false);
-    Matrix_design=inputdlgcol(prompt,'Provide Matrix Design',[1,34],Table_base,AddOpts,N_Channels);
+    Matrix_design = inputdlgcol(prompt,'Provide Matrix Design',[1,34],Table_base,AddOpts,N_Channels);
     Matrix_design = reshape(Matrix_design,N_Rounds,N_Channels);
     Matrix_design =  cell2table(Matrix_design);
     
@@ -39,10 +36,7 @@ if ~ isempty(answer)
     Matrix_design.Properties.VariableNames = names_col;
     
 else
-    disp('Parameter definition canceled')
-    Parameters = {};
+    disp('Matrix design canceled')
+    Matrix_design = {};
 end
-    
 
-    
-end
